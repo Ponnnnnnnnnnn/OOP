b3c92f1b3d628bee83b88abda189e06f30abe0af
Ponnnnnnnnnnn <khamwang.tanakorn@gmail.com>
2024-07-11 10:25:24 +0700

    Lab2

---
+++ Lab2/2.1.py
@@ -0,0 +1,27 @@
+for i in range(1, 6, +1):
+    print('*' * i)
+print()
+    
+for i in range(5, 0, -1):
+    print('*' * i)
+print()
+
+for i in range(1, 6, +1):
+    print(' ' * (5 - i), end='')
+    print('*' * (2 * i - 1))
+print()
+
+for i in range(5, 0, -1):
+    print(' ' * (5 - i), end='')
+    print('*' * (2 * i - 1))
+print()
+
+for i in range(1, 6, +1):
+    print(' ' * (5 - i), end='')
+    print('*' * i)
+print()
+
+for i in range(5, 0, -1):
+    print(' ' * (5-i), end='')
+    print('*' * i)
+print()
