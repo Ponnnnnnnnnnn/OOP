5c9d8f7ace1ad2d7b743f152f35ae636b0c6863b
Ponnnnnnnnnnn <khamwang.tanakorn@gmail.com>
2024-10-03 11:31:56 +0700

    Add files via upload

---
+++ Lab4/Unit6/4-6.2.py
@@ -0,0 +1,14 @@
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+union_set = set1 | set2
+print("union_set:", union_set)
+
+intersection_set = set1 & set2
+print("Intersection_set:", intersection_set)
+
+difference_set = set1 - set2
+print("difference_set:", difference_set)
+
+symmetric_difference_set = set1 ^ set2
+print("symmetric_difference_set:", symmetric_difference_set)
---
+++ Lab4/Unit6/4-6.7.py
@@ -0,0 +1,5 @@
+set1 = {1, 2, 3, 4}
+set2 = {3, 4}
+
+value_superset = set1.issuperset(set2)
+print(value_superset)
